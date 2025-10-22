bd0ce0601eaf97d4c1d8b175792497f19cbd5ba2
REyna9305 <realex2cno.l.30.04@gmail.com>
2025-10-22 13:16:46 -0600

    Update and rename caña.js to cana.js

--- Objetos_js/cana.js
+++ Objetos_js/cana.js
@@ -3,19 +3,20 @@
     export default function Caña1({x,y,z}){
 
     //PALITO DE CAÑA
-    var caña = new THREE.Mesh(
+    var cana = new THREE.Mesh(
     new THREE.CylinderGeometry(0.5, 0.5, 5, 15), // radio superior, radio inferior, altura, segmentos
     new THREE.MeshLambertMaterial({ color: 0x8CD166 }) // color café madera
     );
-    caña.position.set(0, 3, 0); // 3 = mitad de la altura para que toque el piso
+    cana.position.set(0, 3, 0); // 3 = mitad de la altura para que toque el piso
     //scene.add(caña);
-    caña.castShadow = true;
-    caña.receiveShadow = true;
+    cana.castShadow = true;
+    cana.receiveShadow = true;
 
     //CañaGrup
-    var CañaGrup = new THREE.Group();
-    CañaGrup.add(caña);
+    var CanaGrup = new THREE.Group();
+    CanaGrup.add(caña);
 
-    CañaGrup.position.set(x,y,z);
-    return CañaGrup;
+    CanaGrup.position.set(x,y,z);
+    return CanaGrup;
+
     }
